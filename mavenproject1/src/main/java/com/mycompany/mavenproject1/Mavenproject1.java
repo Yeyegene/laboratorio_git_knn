a62e2e3466d07ca7c793ed826bc41d877ad276ea
Yeyegene <genesis.salgadom@gmail.com>
2025-09-16 19:39:29 -0600

    mavenproject1

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author genes
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+       int numero =7;
+       System.out.println("Soy un numero: " + numero);
+    }
+}
